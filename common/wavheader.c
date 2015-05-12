f7ff8e01dca24d1eae8235fe7dcbb21ab35dcacc
Ian Littke <ian.littke@gmail.com>
2015-05-12 00:32:13 -0700

    Big updates to the wav reader code **This will break Chris** But we should only read in the entire file once, not section by section. This is due to forthcoming merging in with the microcontroller Where the data will be passed through to the code instead of accessed ...plus it makes the code usable for other projects =D

--- common/wavheader.c
+++ common/wavheader.c
@@ -1,118 +1,111 @@
 #include "wavheader.h"
 
-
-struct WaveHeader *headerCreate()
-{
+struct WaveHeader *headerCreate() {
     struct WaveHeader *wav = malloc(sizeof(struct WaveHeader));
     return wav;
 }
 
-void printHeader(struct WaveHeader *header)
-{
+void printHeader(struct WaveHeader *header) {
     unsigned int i = 0;
-    for (i = 0; i < sizeof(header->chunk_id); i++)
+    for (i = 0; i < sizeof(header->chunkId); i++)
     {
-      printf("%c", header->chunk_id[i]);
+      printf("%c", header->chunkId[i]);
     }
     printf("\n");
-    printf("%d\n", header->chunk_size+8);
+    printf("%d\n", header->chunkSize+8);
     printf("wav ID: ");
-      for (i = 0; i < sizeof(header->chunk_id); i++)
+      for (i = 0; i < sizeof(header->chunkId); i++)
     {
       printf("%c", header->wavID[i]);
     }
     printf("\n");
     printf("Fmt ID: ");
-      for (i = 0; i < sizeof(header->chunk_id); i++)
+      for (i = 0; i < sizeof(header->chunkId); i++)
     {
       printf("%c", header->fmtID[i]);
     }
     printf("\n");
-        printf("fmt size: %d\n", header->fmtchunk_size);
+        printf("fmt size: %d\n", header->fmtChunkSize);
     printf("format tag: %hu\n", header->wFormatTag);
     printf("N channels: %hu\n", header->nChannels);
-    printf("Sample per sec: %d\n", header->sample_rate);
-    printf("bytes per sec: %d\n", header->byte_rate);
-    printf("blockalign: %d\n", header->block_align);
+    printf("Sample per sec: %d\n", header->sampleRate);
+    printf("bytes per sec: %d\n", header->byteRate);
+    printf("blockalign: %d\n", header->blockAlign);
     printf("bits per sample: %d\n", header->bps);
 
-    for (i = 0; i < sizeof(header->chunk_id); i++)
+    for (i = 0; i < sizeof(header->chunkId); i++)
     {
-      printf("%c", header->datachunk_id[i]);
+      printf("%c", header->datachunkId[i]);
     }
-    printf("\r\nData size: %d\n", header->datachunk_size);
+    printf("\r\nData size: %d\n", header->datachunkSize);
     return;
 }
 
-struct WaveHeader *readHeader(FILE *fp, struct WaveHeader *header)
-{
+int readHeader(FILE *fp, struct WaveHeader *header) {
   char name[4];
-  int result;
   int cont = 0;
 
   if (fp)
   {
-    fread(&header->chunk_id, sizeof(char), 4, fp); //read in first four bytes
-    fread(&header->chunk_size, sizeof(int), 1, fp); //read in 32bit size value
+    fread(&header->chunkId, sizeof(char), 4, fp); //read in first four bytes
+
+    if (header->chunkId[0] != 'R' || header->chunkId[1] != 'I' || header->chunkId[2] != 'F' || header->chunkId[3] != 'F')
+      return 1;
+
+
+    fread(&header->chunkSize, sizeof(int), 1, fp); //read in 32bit size value
 
     fread(&header->wavID, sizeof(char), 4, fp);
     fread(&header->fmtID, sizeof(char), 4, fp);
-    fread(&header->fmtchunk_size, sizeof(unsigned int), 1, fp);
+    fread(&header->fmtChunkSize, sizeof(unsigned int), 1, fp);
     fread(&header->wFormatTag, sizeof(unsigned short int), 1, fp);
     fread(&header->nChannels, sizeof(short int), 1, fp);
-    fread(&header->sample_rate, sizeof(int), 1, fp);
-    fread(&header->byte_rate, sizeof(int), 1, fp);
-    fread(&header->block_align, sizeof(short int), 1, fp);
+    fread(&header->sampleRate, sizeof(int), 1, fp);
+    fread(&header->byteRate, sizeof(int), 1, fp);
+    fread(&header->blockAlign, sizeof(short int), 1, fp);
     fread(&header->bps, sizeof(short int), 1, fp);
     //data chucnk should be the next one
     //implement a catch just in case
+    //this hasn't been tested yet...
     fread(&name, sizeof(char), 4, fp);
     while (!cont) {
-
       if (name[0] != 'd' || name[1] != 'a' || name[2] != 't' || name[3] != 'a') {
         printf("**%s\n", name);
         advanceToNextChunk(fp);
         fread(&name, sizeof(char), 4, fp);
       }
       else
-          cont = 1;
+        cont = 1;
     }
     fseek(fp,-4,SEEK_CUR);
-    fread(&header->datachunk_id, sizeof(char),4,fp);
-    fread(&header->datachunk_size, sizeof(int),1,fp);
+    fread(&header->datachunkId, sizeof(char),4,fp);
+    fread(&header->datachunkSize, sizeof(int),1,fp);
   }
-  return header;
+  header->totalSamples = header->datachunkSize / (header->nChannels * (header->bps / 8));
+
+  return 0;
 }
 
-void readDataIntoBuffer(float *b, FILE *fp)
-{
-    readAmountIntoBuffer(512, b, fp);
-}
-
-
-void advanceToNextChunk(FILE *fp)
-{
+void advanceToNextChunk(FILE *fp) {
   int size = 0;
   fread(&size, sizeof(int), 1, fp);
+
   fseek(fp,size,SEEK_CUR);
 }
 
-void readAllData(FILE *fp, struct WaveHeader *header)
-{
+int readAllData(FILE *fp, struct WaveHeader *header) {
   int i = 0;
-  int numOfSamples = 0;
-  numOfSamples = header->datachunk_size / (header->nChannels * (header->bps / 8));
 
 
-  header->chan1 = malloc(numOfSamples  * sizeof(short int));
+    header->chan1 = malloc(header->totalSamples  * sizeof(short int));
   if (header->nChannels > 1)
-    header->chan2 = malloc((numOfSamples  * sizeof(short int)));
+    header->chan2 = malloc((header->totalSamples  * sizeof(short int)));
   if (header->nChannels > 2)
-    header->chan3 = malloc((numOfSamples  * sizeof(short int)));
+    header->chan3 = malloc((header->totalSamples  * sizeof(short int)));
   if (header->nChannels > 3)
-    header->chan4 = malloc((numOfSamples  * sizeof(short int)));
+    header->chan4 = malloc((header->totalSamples  * sizeof(short int)));
 
-  for (i = 0; i < header->datachunk_size / header->block_align; i++) {
+  for (i = 0; i < header->datachunkSize / header->blockAlign; i++) {
     fread(&header->chan1[i],1,2,fp);
     if (header->nChannels > 1)
       fread(&header->chan2[i],1,2,fp);
@@ -121,18 +114,7 @@ void readAllData(FILE *fp, struct WaveHeader *header)
     if (header->nChannels > 3)
       fread(&header->chan4[i],1,2,fp);
   }
-}
-
-void readAmountIntoBuffer(int samples, float *b, FILE *fp)
-{
-    int i = 0;
-    short int sample = 0;
-    for (i = 0; i < samples; i++)
-    {
-        fread(&sample, 1, 2, fp);
-        b[i] = sample;
-    }
-    return;
+  return 0;
 }
 
 void freeChannelMemory(struct WaveHeader *wav)
--- common/wavheader.h
+++ common/wavheader.h
@@ -1,20 +1,25 @@
+#ifndef wav_header_h
+#define wav_header_h
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 struct WaveHeader{
-    char               chunk_id[4];
-    int                chunk_size;
+    char               wavName[300];
+    char               chunkId[4];
+    int                chunkSize;
     char               wavID[4];
     char               fmtID[4];
-    unsigned short int fmtchunk_size;
+    unsigned short int fmtChunkSize;
     unsigned short int wFormatTag;
-    short int         nChannels;
-    int               sample_rate;
-    int               byte_rate;
-    short int         block_align;
-    short int         bps;
-    char               datachunk_id[4];
-    unsigned int      datachunk_size;
+    short int          nChannels;
+    int                sampleRate;
+    int                byteRate;
+    short int          blockAlign;
+    short int          bps;
+    char               datachunkId[4];
+    unsigned int       datachunkSize;
+    unsigned int       totalSamples;
     short int * chan1;
     short int * chan2;
     short int * chan3;
@@ -22,10 +27,9 @@ struct WaveHeader{
 };
 
 
-void readDataIntoBuffer(float *b, FILE *fp);
-void readAmountIntoBuffer(int samples, float *b, FILE *fp);
-struct WaveHeader *readHeader(FILE *fp, struct WaveHeader *header);
+int readHeader(FILE *fp, struct WaveHeader *header);
 void printHeader(struct WaveHeader *header);
 void advanceToNextChunk(FILE *fp);
-void readAllData(FILE *fp, struct WaveHeader *header);
+int readAllData(FILE *fp, struct WaveHeader *header);
 void freeChannelMemory(struct WaveHeader *wav);
+#endif
